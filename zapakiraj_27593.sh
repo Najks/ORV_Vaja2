113ec41595badcb64169672ee90afd19ce46dac3
vboxuser <vboxuser@Runnerji.myguest.virtualbox.org>
2024-04-13 23:57:23 +0200

    Popravljen run scripte5

--- zapakiraj_27593.sh
+++ zapakiraj_27593.sh
@@ -3,10 +3,10 @@
 # Postavimo se v direktorij, kjer se nahaja Dockerfile
 cd /home/vboxuser/ORV_Vaja2
 # Zgradimo Docker sliko
-docker build . --file Dockerfile --tag najks/orv_vaja2:latest
+sudo docker build . --file Dockerfile --tag najks/orv_vaja2:latest
 
 # Avtentificiramo se na Docker Hub
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
 # Potisnemo Docker sliko na Docker Hub
-docker push najks/orv_vaja2:latest
+sudo docker push najks/orv_vaja2:latest
